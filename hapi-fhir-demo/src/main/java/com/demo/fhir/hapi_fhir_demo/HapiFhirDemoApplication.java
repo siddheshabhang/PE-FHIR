1d2a555ddec3fc23503342518a743c4e5b40cfbe
siddheshabhang <1908siddhesh@gmail.com>
2026-01-31 14:34:00 +0530

    Initial project setup

---
+++ hapi-fhir-demo/src/main/java/com/demo/fhir/hapi_fhir_demo/HapiFhirDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.fhir.hapi_fhir_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HapiFhirDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HapiFhirDemoApplication.class, args);
+	}
+
+}
---
+++ hapi-fhir-demo/src/test/java/com/demo/fhir/hapi_fhir_demo/HapiFhirDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.fhir.hapi_fhir_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HapiFhirDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
